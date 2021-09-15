681ea59f3df3c7cb0da519c38e621111f0ad4ddd
tomascabrerizo <tomascabrerizo97@gmail.com>
2021-09-15 20:59:04 -0300

    debug dynamic array

--- tlib_darray.h
+++ tlib_darray.h
@@ -1,23 +1,63 @@
 #ifndef DARRAY_H
 #define DARRAY_H
 
-#define DArraySizePtr(dArray) (((size_t *)dArray) - 2)
-#define DArrayCapPtr(dArray)  (((size_t *)dArray) - 1)
+#include <string.h>
 
-void *PushDArray(void *dArray, size_t size)
+#define DArraySizePtr(dArray) (((size_t *)(dArray)) - 2)
+#define DArrayCapPtr(dArray)  (((size_t *)(dArray)) - 1)
+
+size_t DArraySize(void *dArray)
 {
-    if(!dArray)
+    return *DArraySizePtr(dArray);
+}
+
+void *DArrayPush(void **dArray, size_t size)
+{
+    if(!*dArray)
     {
         size_t capacity = (sizeof(size_t)*2)+(size*2);
-        dArray = PlatformAllocMemory(capacity);
-        dArray = ((U8 *)dArray) + (sizeof(size_t)*2);
+        *dArray = PlatformAllocMemory(capacity);
+        *dArray = ((U8 *)*dArray) + (sizeof(size_t)*2);
 
-        *DArrayCapPtr(dArray) = capacity;
-        *DArraySizePtr(dArray) = size;
+        *DArrayCapPtr(*dArray) = capacity;
+        *DArraySizePtr(*dArray) = size;
+        return *dArray;
     }
-    
+    else if((*DArraySizePtr(*dArray) + size) >= *DArrayCapPtr(*dArray))
+    {
+        size_t dArraySize = *DArraySizePtr(*dArray);
+        void *oldMem = ((U8 *)*dArray) - (sizeof(size_t)*2);
+        
+        // NOTE: Allocate a bigger array and copy the old in the new one
+        size_t newCapacity = (sizeof(size_t)*2)+dArraySize*2;
+        void * newDArray = PlatformAllocMemory(newCapacity);
+        newDArray = ((U8 *)newDArray) + (sizeof(size_t)*2);
+        memcpy(newDArray, *dArray, dArraySize);
+        *dArray = newDArray;
+        // NOTE: Release the old array
+        PlatformReleaseMemory(oldMem);
+        
+        void *result = (U8 *)(*dArray) + dArraySize;
 
-    return ((U8 *)dArray) + *DArraySizePtr(dArray);
+        // NOTE: Set the new size and capacity
+        *DArrayCapPtr(*dArray) = newCapacity;
+        *DArraySizePtr(*dArray) = dArraySize + size;
+        
+        return result;
+    }
+    else
+    {
+        void *result = (U8 *)(*dArray) + *DArraySizePtr(*dArray);
+        *DArraySizePtr(*dArray) += size;
+        return result;
+    }
+}
+
+void DArrayRelease(void **dArray)
+{
+    void *mem = ((U8 *)(*dArray)) - (sizeof(size_t)*2);
+    PlatformReleaseMemory(mem);
+    *dArray = 0;
 }
 
 #endif // DARRAY_H
--- tlib_main.c
+++ tlib_main.c
@@ -3,6 +3,7 @@
 #include "tlib_string.h"
 #include "tlib_render.h"
 #include "tlib_mesh.h"
+#include "tlib_darray.h"
 
 #define NUMBER_OF_STARS 512
 
@@ -123,10 +124,32 @@ void GameInit(BackBuffer *buffer)
     testMesh = LoadObjFile(&gameArena, "data/monkey.obj");   
 
     StarFieldInit(&gameStarField);
+    
 }
 
 void GameUpdateAndRender(BackBuffer *buffer, F32 dt)
 {
+
+    // NOTE: Dynamic array test
+    {
+        V3F32 *vertices = 0;
+        for(U32 i = 0; i < 100; ++i)
+        {
+            V3F32 *vertex = (V3F32 *)DArrayPush(&vertices, sizeof(V3F32));
+            *vertex = _V3F32(RandomF32(), RandomF32(), RandomF32());
+            int breakHere = 0;
+            (void)breakHere;
+        }
+#if 0
+        for(U32 i = 0; i < DArraySize(vertices)/sizeof(V3F32); ++i)
+        {
+            printf("%d) (x:%f, y:%f, z:%f)\n", i, vertices[i].x, vertices[i].y, vertices[i].z); 
+        }
+#endif   
+        DArrayRelease(&vertices);
+    }
+
+
     // NOTE: Clear the render buffers each frame
     ClearBackBuffer(buffer, 0x00, 0x00, 0x00);
     ClearZBuffer(buffer);
